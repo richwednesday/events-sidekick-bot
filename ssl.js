d322bbfdeb32e736a545c620d55624792881bc2d
richwednesday <iwokjoseph@gmail.com>
2018-04-30 00:37:56 -0700

    setting up ssl

--- ssl.js
+++ ssl.js
@@ -3,14 +3,14 @@ const app = express();
 
 app.get('/health-check', (req, res) => res.sendStatus(200));
 
-// app.use(express.static('static'));
-// app.listen(80);
+app.use(express.static('static'));
+app.listen(80);
 
-const https = require('https');
-const fs = require('fs');
-const options = {
-  key: fs.readFileSync('./sslcertcopy/privKey.pem'),
-  cert: fs.readFileSync('./sslcertcopy/cert.crt'),
-  ca: fs.readFileSync('./sslcertcopy/chain.crt')
-};
-https.createServer(options, app).listen(443, "0.0.0.0");
+// const https = require('https');
+// const fs = require('fs');
+// const options = {
+//   key: fs.readFileSync('./sslcertcopy/privKey.pem'),
+//   cert: fs.readFileSync('./sslcertcopy/cert.crt'),
+//   ca: fs.readFileSync('./sslcertcopy/chain.crt')
+// };
+// https.createServer(options, app).listen(443, "0.0.0.0");
