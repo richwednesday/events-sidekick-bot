a1a56ba920ec7c3d1112b67db7083597262af228
richwednesday <iwokjoseph@gmail.com>
2018-06-27 11:22:37 +0100

    new sidekick

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 const child_process = require("child_process");
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 const express = require('express');
 const Boombot = require('./boombot/boombot')
 
@@ -18,55 +17,32 @@ child_process.fork("cron.js", ['FORKED']);
 // Boombot.PersistentMenu.enable()
 
 // Webserver parameter
-const PORT = process.env.PORT || 5000;
-
-// Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
-if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
-const FB_APP_SECRET = process.env.FB_APP_SECRET;
-if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
-
-let FB_VERIFY_TOKEN = null;
-crypto.randomBytes(8, (err, buff) => {
-  if (err) throw err;
-  FB_VERIFY_TOKEN = buff.toString('hex');
-  console.log(`/webhook will accept the Verify Token "${FB_VERIFY_TOKEN}"`);
-});
+const PORT = process.env.PORT
 
 // Starting our webserver and putting it all together
 const app = express();
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
-
-// Webhook setup
-app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    console.log('Webhook verified')
-    res.sendStatus(400);
-  }
-});
+app.use(bodyParser.json())
 
 
-app.post('/webhook', function (req, res) {
+app.post('/webhook', (req, res) => {
   let data = req.body;
 
   if (data.object == 'page') {    
-    data.entry.forEach(function(pageEntry) {
-      let pageID = pageEntry.id;
-      let timeOfEvent = pageEntry.time;
-
+    data.entry.forEach((pageEntry) => {
+      
       // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.message) {
-          Boombot.MessageDispatch(messagingEvent);
-        } else if (messagingEvent.postback) {
-          Boombot.PostbackDispatch(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
+      if (pageEntry.messaging) { 
+        pageEntry.messaging.forEach((messagingEvent) => {
+          if (messagingEvent.message) {
+            Boombot.MessageDispatch(messagingEvent);
+          } else if (messagingEvent.postback) {
+            Boombot.PostbackDispatch(messagingEvent);
+          } else {
+            console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+          }
+        });
+      }
+      else console.log("Webhook received unknown Event: ", pageEntry)
     });
 
     res.sendStatus(200);
@@ -74,35 +50,5 @@ app.post('/webhook', function (req, res) {
 
 });
 
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-function verifyRequestSignature(req, res, buf) {
-  let signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    // For testing, let's log an error. In production, you should throw an
-    // error.
-    console.error("Couldn't validate the signature.");
-  } else {
-    let elements = signature.split('=');
-    let method = elements[0];
-    let signatureHash = elements[1];
-
-    let expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
-                        .update(buf)
-                        .digest('hex');
-
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
-
 app.listen(PORT);
-console.log('Listening on :' + PORT + '...');
+console.log('Developer Events listening on :' + PORT + '...');
