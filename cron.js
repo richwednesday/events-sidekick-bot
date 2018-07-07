239895e32660ab3ce1d5035ebb52af74dbfd4c3c
richwednesday <iwokjoseph@gmail.com>
2018-07-07 07:32:34 +0100

    success is relative

--- cron.js
+++ cron.js
@@ -2,17 +2,20 @@ const schedule = require('node-schedule')
 const store = require('./boombot/store')
 const search = require('./commands/search')
 
-const eventsSchedule = schedule.scheduleJob('15 42 6 * * 6', () => {
+
+schedule.scheduleJob('15 42 6 * * 6', () => {
   store.getSubscribers(reply => {
     console.log(reply)
     
     reply.forEach(id => {
       store.getLocation(id, (location) => {
+        console.log(location)
+
         if (location) {
           search.sendSubscribedMessage(id)
           search.sendEvents(id, JSON.parse(location))
         } 
-        else { this.getLocation(id) }  
+        // else { this.getLocation(id) }  
       }) 
     })
   }) 	
